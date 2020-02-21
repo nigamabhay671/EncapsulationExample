becc2d62f7c6f5df85d69ac7fe169b905b2a7c05
nigamabhay671 <nigamabhay671@users.noreply.github.com>
2020-02-21 23:21:01 +0530

    Add project files.

---
+++ EncapsulationExample/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncapsulationExample
+{
+    class Employee
+    {
+        private int salary;
+        private string name;
+        private int workingHours;
+        private int perhourSalary = 100;
+
+        public string GetName()
+        {
+            return name;
+        }
+        public void SetName(string Name)
+        {
+            name = Name;
+        }
+        public int GetHours()
+        {
+            return workingHours;
+        }
+        public void SetHours(int WorkingHours)
+        {
+            workingHours = WorkingHours;
+        }
+        public int CalculateSalary()
+        {
+            return workingHours * perhourSalary;
+        }
+    }
+}
---
+++ EncapsulationExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EncapsulationExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.SetName("Abhay");
+            emp.SetHours(8);
+
+            Console.WriteLine("The Salary For:-" + emp.GetName() + "=" + emp.CalculateSalary());
+        }
+    }
+}
